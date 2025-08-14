44162a06268569586d559a0c337a5c75f87d8ad7
Binada <binada2005@gmail.com>
2025-08-15 00:36:15 +0530

    Enable App Router in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // ✅ enable App Router
+  },
+};
 
 export default nextConfig;
